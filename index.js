5c66787cd18faa0abf2a32fdfc9199808b8d3866
mzlatev@unionproject.eu <mzlatev@unionproject.eu>
2021-07-15 18:46:23 +0300

    fixed small bug

--- index.js
+++ index.js
@@ -13,34 +13,34 @@ const port = 3001;
 app.use(cors({ origin: "*" }));
 
 // File upload endpoint
-app.post("/upload", upload.single("file"), function(req, res, next) {
-    const file = req.file;
-    if (!file) {
-        res.status(400).json({ error: "No file uploaded" });
-    }
+app.post("/upload", upload.single("file"), function (req, res, next) {
+  const file = req.file;
+  if (!file) {
+    res.status(400).json({ error: "No file uploaded" });
+  }
 
-    parser
-        .getEmployeeData(`./uploads/${file.filename}`)
-        .then((data) => res.json({ rows: data }))
-        .catch((err) => {
-            res.status(400).json({ error: err });
-        })
-        .finally(() => {
-            fs.unlink(`./uploads/${file.filename}`, function(error) {
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-            });
-        });
+  parser
+    .getEmployeeData(`./uploads/${file.filename}`)
+    .then((data) => res.json({ rows: data }))
+    .catch((err) => {
+      res.status(400).json({ error: err });
+    })
+    .finally(() => {
+      fs.unlink(`./uploads/${file.filename}`, function (error) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+      });
+    });
 });
 
 // Serve react build
 app.use(express.static(path.join(__dirname, "build")));
 app.use("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "build", "index.html"))
+  res.sendFile(path.join(__dirname, "build", "index.html"))
 );
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
--- script.js
+++ script.js
@@ -11,60 +11,68 @@ const sqlite3 = require("sqlite3").verbose();
  */
 
 function getEmployeeData(inputFile) {
-    return new Promise((resolve, reject) => {
-        if (!fs.existsSync(inputFile)) {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(inputFile)) {
+      reject(error);
+    }
+
+    const db = new sqlite3.Database(":memory:", (error) => {
+      if (error) {
+        reject(error);
+      }
+
+      console.log("In-memory SQLite Database Connection Opened");
+    });
+
+    db.serialize(() => {
+      db.run("DROP TABLE IF EXISTS Employees").run(
+        "CREATE TABLE Employees(EmpID INTEGER, ProjectID INTEGER, DateFrom TEXT, DateTo TEXT)",
+        function (error) {
+          if (error) {
             reject(error);
+          }
+          console.log("Employees Table Created Successfully");
         }
+      );
 
-        const db = new sqlite3.Database(":memory:", (error) => {
-            if (error) {
-                reject(error);
-            }
-
-            console.log("In-memory SQLite Database Connection Opened");
-        });
-
-        db.serialize(() => {
-            db.run("DROP TABLE IF EXISTS Employees").run(
-                "CREATE TABLE Employees(EmpID INTEGER, ProjectID INTEGER, DateFrom TEXT, DateTo TEXT)",
-                function(error) {
-                    if (error) {
-                        reject(error);
-                    }
-                    console.log("Employees Table Created Successfully");
-                }
-            );
-
+      db.serialize(() => {
+        fs.createReadStream(inputFile)
+          .pipe(
+            parse({
+              delimiter: ",",
+            })
+          )
+          .on("data", function (csvrow) {
             db.serialize(() => {
-                fs.createReadStream(inputFile)
-                    .pipe(
-                        parse({
-                            delimiter: ",",
-                        })
-                    )
-                    .on("data", function(csvrow) {
-                        db.serialize(() => {
-                            csvrow = csvrow.map((col) => col.trim());
+              csvrow = csvrow.map((col) => col.trim());
 
-                            if (csvrow[3] === "NULL") {
-                                csvrow[3] = new Date().toISOString().split("T")[0];
-                            }
+              /* csvrow[2] = new Date(Date.parse(csvrow[2]))
+                .toISOString()
+                .split("T")[0]; */
 
-                            db.run(
-                                `INSERT INTO Employees(EmpID, ProjectID, DateFrom, DateTo) values(?, ?, ?, ?)`,
-                                csvrow,
-                                function(error) {
-                                    if (error) {
-                                        reject(error);
-                                    }
-                                }
-                            );
-                        });
-                    })
-                    .on("end", function() {
-                        /* e1.datefrom <= e2.dateto AND e1.dateto >= e2.datefrom AND */
-                        db.all(
-                            `SELECT 
+              if (csvrow[3] === "NULL") {
+                csvrow[3] = new Date().toISOString().split("T")[0];
+              } else {
+                /* csvrow[3] = new Date(Date.parse(csvrow[3]))
+                  .toISOString()
+                  .split("T")[0]; */
+              }
+
+              db.run(
+                `INSERT INTO Employees(EmpID, ProjectID, DateFrom, DateTo) values(?, ?, ?, ?)`,
+                csvrow,
+                function (error) {
+                  if (error) {
+                    reject(error);
+                  }
+                }
+              );
+            });
+          })
+          .on("end", function () {
+            /* e1.datefrom <= e2.dateto AND e1.dateto >= e2.datefrom AND */
+            db.all(
+              `SELECT 
                 e1.empid AS EmpID1, 
                 e2.empid AS EmpID2, 
                 e1.projectid AS ProjectID,
@@ -73,67 +81,68 @@ function getEmployeeData(inputFile) {
               ON e1.projectid = e2.projectid AND 
                 EmpID1 > EmpID2
               WHERE e1.empid != e2.empid
-              ORDER BY DaysWorked DESC;`, [],
+              ORDER BY DaysWorked DESC;`,
+              [],
 
-                            (error, rows) => {
-                                if (error) {
-                                    console.log(error);
-                                    reject(error);
-                                }
+              (error, rows) => {
+                if (error) {
+                  console.log(error);
+                  reject(error);
+                }
 
-                                console.log("Table populated from file successfully");
-                                resolve(rows);
-                            }
-                        );
-                    })
-                    .on("error", function(error) {
-                        console.log(error);
-                        reject("Error parsing file");
-                    });
-            });
-        });
+                console.log("Table populated from file successfully");
+                resolve(rows);
+              }
+            );
+          })
+          .on("error", function (error) {
+            console.log(error);
+            reject("Error parsing file");
+          });
+      });
     });
+  });
 }
 
 async function main() {
-    if (process.argv.length < 3) {
-        console.log("Usage node Employees.ts [filename]");
-        process.exit();
+  if (process.argv.length < 3) {
+    console.log("Usage node Employees.ts [filename]");
+    process.exit();
+  }
+
+  const filePath = process.argv[2];
+
+  try {
+    const results = await getEmployeeData(filePath, true);
+    const map = {};
+    const highest = {
+      firstEmployeeId: -1,
+      secondEmployeeId: -1,
+      daysWorked: -1,
+    };
+
+    for (const row of results) {
+      const mapKey = `${row.EmpID1}-${row.EmpID2}`;
+      if (!map[mapKey]) {
+        map[mapKey] = 0;
+      }
+      map[mapKey] += row.DaysWorked;
+      if (map[mapKey] > highest.daysWorked) {
+        highest.firstEmployeeId = row.EmpID1;
+        highest.secondEmployeeId = row.EmpID2;
+        highest.daysWorked = map[mapKey];
+      }
     }
 
-    const filePath = process.argv[2];
-
-    try {
-        const results = await getEmployeeData(filePath, true);
-        const map = {};
-        const highest = {
-            firstEmployeeId: -1,
-            secondEmployeeId: -1,
-            daysWorked: -1,
-        };
-
-        for (const row of results) {
-            const mapKey = `${row.EmpID1}-${row.EmpID2}`;
-            if (!map[mapKey]) {
-                map[mapKey] = 0;
-            }
-            map[mapKey] += row.DaysWorked;
-            if (map[mapKey] > highest.daysWorked) {
-                highest.firstEmployeeId = row.EmpID1;
-                highest.secondEmployeeId = row.EmpID2;
-                highest.daysWorked = map[mapKey];
-            }
-        }
-
-        console.log(highest);
-        return highest;
-    } catch (error) {
-        console.log(error);
-    }
+    console.log(highest);
+    return highest;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 if (require.main === module) {
-    main();
+  main();
 }
 
 exports.getEmployeeData = getEmployeeData;
